4af377b4e5136d2c80b301584b331c3b804dde08
sjlee3157 <sjlee3157@gmail.com>
2018-11-15 00:46:37 -0800

    Added Intro to JS exercises

---
+++ js-exercises.js
@@ -0,0 +1,26 @@
+// Intro to JS
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/welcome-to-js.md
+
+// Linter setup
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/exercises/setup-linting.md
+
+// Ruby to JS worksheet
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/exercises/ruby-to-js-worksheet.md
+
+// Fun with functions! lesson:
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/fun-with-functions.md
+
+// JS Functions Worksheet:
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/exercises/functions-worksheet.md
+
+// Arrow Functions lesson:
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/arrow-functions.md
+
+// Jest testing
+// https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/10-JavaScript/testing.md
+
+// Adagrams I:
+// https://github.com/AdaGold/adagrams#readme
+
+// MDN Reintroduction to JS tutorial:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
