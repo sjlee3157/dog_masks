64cae5391655ef3689a6e1ff0bd1adc7d1df68ce
sjlee3157 <sjlee3157@gmail.com>
2018-11-15 01:20:47 -0800

    Added intro js exercises and gitignore

--- jsexercises.js
+++ jsexercises.js
@@ -24,3 +24,85 @@
 
 // MDN Reintroduction to JS tutorial:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/A_re-introduction_to_JavaScript
+
+// Ruby to JS problem set!
+
+let personAge = 55;
+let adaAge = 2;
+
+if (personAge < adaAge) {
+  console.log('This person is younger');
+} else if (adaAge < personAge) {
+  console.log('Ada is younger');
+} else {
+  console.log("They're the same!")
+}
+
+//
+
+let x = 7;
+let y = 7;
+
+if (x > y || x === y) {
+  if (x > y) {
+    console.log('x is bigger');
+  } else {
+    console.log('x = y');
+  }
+} else {
+  console.log('y is bigger');
+}
+
+//
+
+for (let i = 0; i < 10; i++) {
+  console.log(i*i);
+}
+
+//
+
+let total = 0;
+let range = [1,2,3];
+
+for (let elm of range) {
+  total = total + elm;
+}
+console.log(total);
+
+//
+
+let i = 0;
+while (i < 3) {
+  console.log('hi');
+  i = i + 1;
+}
+
+console.log('bye');
+
+//
+
+let fruits = ["banana", "apple", "kiwi"];
+for (let fruit of fruits) {
+  console.log(`I love ${fruit}!`);
+}
+
+//
+
+let anotherTotal = 0;
+let values = [4, 6, 2, 8, 11];
+for (let value of values) {
+  anotherTotal = anotherTotal + value;
+}
+
+console.log(anotherTotal);
+
+//
+
+let moreValues = [8, 5, 3, 10, 14, 2];
+for (let value of moreValues) {
+  if (value === 10) {
+    console.log('Special case!');
+  } else {
+    console.log(`Regular values like ${value}!`);
+  }
+}
