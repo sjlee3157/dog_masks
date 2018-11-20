645c5f0035312750a9fbe6784997854c19b59df9
sjlee3157 <sjlee3157@gmail.com>
2018-11-20 15:27:54 -0800

    working GET and POST with html form

--- JS-Axios/index.js
+++ JS-Axios/index.js
@@ -29,7 +29,7 @@ const loadPets = () => {
     .then((response) => {
       reportStatus(`Successfully loaded ${response.data.length} pets`);
       response.data.forEach((pet) => {
-        petList.append(`<li>${pet.name}<ul><li>${pet.id}</li><li>${pet.owner}</li><li>${pet.age}</li></ul></li>`);
+        petList.append(`<li>${pet.name}</li>`);
       });
     })
     .catch((error) => {
@@ -40,6 +40,29 @@ const loadPets = () => {
   // this works because of asynchronicity?!
 };
 
+const readFormData = () => {
+  let petForm = document.getElementById('pet-form');
+  const petData = new FormData(petForm);
+
+  // const curData = $(`#pet-form input[name="${curInput}"]`).val();
+
+
+  return petData;
+};
+
+const clearForm = () => {
+  $("#pet-form")[0].reset();
+}
+  // console.log($('#pet-form'));
+  // console.log($('#pet-form')[0][0].value);
+
+  // $('#pet-form').each(function() {
+  //   this.reset();
+  // });
+  // $(`#pet-form input[name="name"]`).val('');
+  // $(`#pet-form input[name="age"]`).val('');
+  // $(`#pet-form input[name="owner"]`).val('');
+
 const createPet = (event) => {
   // Note that createPet is a handler for a `submit`
   // event, which means we need to call `preventDefault`
@@ -49,16 +72,17 @@ const createPet = (event) => {
   reportStatus('Sending pet data...');
 
   // Later we'll read these values from a form
-  const petData = {
-    name: 'Nymphadora Tonks!',
-    age: 77,
-    owner: 'samjo'
-  };
+  // const petData = {
+  //   name: 'Nymphadora Tonks!',
+  //   age: 77,
+  //   owner: 'samjo'
+  // };
 
-  axios.post(URL, petData)
+  axios.post(URL, readFormData())
     .then((response) => {
       console.log(response);
       reportStatus(`Successfully added a pet with ID ${response.data.id}!`);
+      clearForm();
     })
     .catch((error) => {
       console.log(error.response);
